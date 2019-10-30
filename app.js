d78ee5a17c4bc6761beec5269f28c838584b80f6
Madison Stehle <mnstehle3@gmail.com>
2019-10-29 19:15:50 -0700

    added first var, prompt, alert in js, added text outline for html

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('This works!');
+// This line of code was to test that the js was linked to the html
+// console.log('This works!');
+
+var bornInSeattle = prompt('Was I born and raised in Seattle?');
+console.log(bornInSeattle);
+bornInSeattle = bornInSeattle.toLowerCase();
+
+if (bornInSeattle === 'yes' || bornInSeattle === 'y') {
+    alert('Not quite! I was actually born in Louisiana, moved to Missouri and went to college, then picked up and moved to Seattle!');
+} else {
+    alert('Bingo, you are right! I was actually born in Louisiana, moved to Missouri and went to college, then picked up and moved to Seattle!');
+}
