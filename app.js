01ea7fe143d861ff71d067de6b64bab1ddfbc2e5
Madison Stehle <mnstehle3@gmail.com>
2019-10-29 19:26:01 -0700

    added 2nd question var, prompt, alert to js and tested

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ console.log(bornInSeattle);
 bornInSeattle = bornInSeattle.toLowerCase();
 
 if (bornInSeattle === 'yes' || bornInSeattle === 'y') {
-    alert('Not quite! I was actually born in Louisiana, moved to Missouri and went to college, then picked up and moved to Seattle!');
+    console.log('Wrong');
+ //   alert('Not quite! I was actually born in Louisiana, moved to Missouri and went to college, then picked up and moved to Seattle!');
 } else {
-    alert('Bingo, you are right! I was actually born in Louisiana, moved to Missouri and went to college, then picked up and moved to Seattle!');
+    console.log('Right');
+ //   alert('Bingo, you are right! I was actually born in Louisiana, moved to Missouri and went to college, then picked up and moved to Seattle!');
+}
+
+var havePets = prompt('Do I have any pets?');
+console.log(havePets);
+havePets = havePets.toLowerCase();
+
+if (havePets === 'yes' || havePets === 'y') {
+    console.log('Right');
+    // alert('You got it! I have two cats named Friedrich and Jacques.');
+} else {
+    console.log('Wrong');
+    //alert('Almost! I have two cats named Friedrich and Jacques.');
 }
